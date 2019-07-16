de2d2b8929bd491905d21816d8ca1b4a52bee5c5
BeornPL <37457935+BeornPL@users.noreply.github.com>
2019-07-16 21:11:43 +0200

    24.3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,11 +1,16 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.controller.TaskController;
+import com.crud.tasks.domain.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MailCreatorService {
@@ -14,10 +19,19 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     @Autowired
+    private TaskController taskController;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/crud");
@@ -25,6 +39,19 @@ public class MailCreatorService {
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("goodbyeMessage", adminConfig.getAdminName());
         context.setVariable("companyDetails", adminConfig.getCompanyName());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String statusEmail(String message) {
+
+        Context context = new Context();
+        //A few variables needed, most important one
+        //uses getTasks().size() method from taskController bean,
+        //and formatting.
+        return templateEngine.process("mail/status-trello-card-mail", context);
     }
 }
